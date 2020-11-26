4feb0540d98a5ba92661534828fc38921bfca4d4
jmal <zhushilun084@gmail.com>
2020-11-27 00:30:09 +0800

    修改：分类管理

--- src/main/java/com/jmal/clouddisk/service/impl/CategoryService.java
+++ src/main/java/com/jmal/clouddisk/service/impl/CategoryService.java
@@ -99,20 +99,15 @@ public class CategoryService {
             }
             return categoryDTO;
         }).collect(Collectors.toList());
-        Map<String, Integer> articleNumMap = null;
-        if (statArticleNum) {
-            articleNumMap = new HashMap<>(16);
-        }
         List<CategoryDTO> categoryTreeList = getSubCategory(null, categoryDTOList);
-        getArticleNum(articleNumMap, categoryTreeList, null);
-        setArticleNum(articleNumMap, categoryTreeList);
         return categoryTreeList;
     }
 
     /**
      * 查找分类的子集
+     *
      * @param parentCategoryId 父分类id
-     * @param categoryDTOList 分类列表
+     * @param categoryDTOList  分类列表
      * @return 分类列表
      */
     private List<CategoryDTO> getSubCategory(String parentCategoryId, List<CategoryDTO> categoryDTOList) {
@@ -130,61 +125,6 @@ public class CategoryService {
             }
             categoryTreeList.add(subCategory);
         });
-        return categoryTreeList;
-    }
-
-    /**
-     * <p>获取文章数</p>
-     * <br>把每个分类的文章数存到articleNumMap里
-     * <br>articleNumMap => key:分类id , value:文章数
-     * @param articleNumMap 每个分类的文章数
-     * @param categoryTreeList 分类树
-     * @param categoryId 分类id
-     * @return 分类树
-     */
-    private int getArticleNum(Map<String, Integer> articleNumMap, List<CategoryDTO> categoryTreeList, String categoryId) {
-        int count = 0;
-        if (articleNumMap == null || categoryTreeList == null) {
-            return count;
-        }
-        for (CategoryDTO category : categoryTreeList) {
-            count += category.getArticleNum();
-            int curCnt = getArticleNum(articleNumMap, category.getChildren(), category.getId());
-            articleNumMap.put(category.getId(), curCnt);
-            count += curCnt;
-        }
-        if(!StringUtils.isEmpty(categoryId)){
-            articleNumMap.put(categoryId, count);
-        }
-        return count;
-    }
-
-    /**
-     * <p>设置文章数</p>
-     * <br>把articleNumMap里的文章数取出来放到categoryTreeList里
-     * @param articleNumMap 每个分类的文章数
-     * @param categoryTreeList 分类树
-     * @return 分类树
-     */
-    private List<CategoryDTO> setArticleNum(Map<String, Integer> articleNumMap, List<CategoryDTO> categoryTreeList){
-        if(articleNumMap == null || categoryTreeList == null){
-            return categoryTreeList;
-        }
-        categoryTreeList = categoryTreeList.stream().peek(category -> {
-            if(category.getChildren() == null || category.getChildren().isEmpty()){
-                category.setArticleNum(category.getArticleNum());
-            } else {
-                if(articleNumMap.containsKey(category.getId())){
-                    category.setArticleNum(articleNumMap.get(category.getId()));
-                } else {
-                    category.setArticleNum(category.getArticleNum());
-                }
-            }
-            List<CategoryDTO> subList = category.getChildren();
-            if(subList != null && !subList.isEmpty()){
-                category.setChildren(setArticleNum(articleNumMap, subList));
-            }
-        }).collect(Collectors.toList());
         return categoryTreeList;
     }
 
