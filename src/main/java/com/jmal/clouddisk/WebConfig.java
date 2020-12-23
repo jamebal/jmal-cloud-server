f123b4506d28edd11a520372374419555d1d9176
jmal <zhushilun084@gmail.com>
2020-12-23 18:04:43 +0800

    优化：加入缓存提升页面加载速度

--- src/main/java/com/jmal/clouddisk/WebConfig.java
+++ src/main/java/com/jmal/clouddisk/WebConfig.java
@@ -3,14 +3,18 @@ package com.jmal.clouddisk;
 import com.jmal.clouddisk.interceptor.AuthInterceptor;
 import com.jmal.clouddisk.interceptor.FileInterceptor;
 import com.jmal.clouddisk.model.FileProperties;
+import com.jmal.clouddisk.util.TimeUntils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 /**
  * WebConfig
@@ -43,7 +47,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/file/**").addResourceLocations(
-                "file:" + fileProperties.getRootDir() + File.separator);
+                "file:" + fileProperties.getRootDir() + File.separator).setCacheControl(CacheControl.maxAge(3600, TimeUnit.SECONDS));
         log.info("静态资源目录:{}", fileProperties.getRootDir() + File.separator);
     }
 }
