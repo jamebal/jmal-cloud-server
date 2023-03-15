eafb3ce4fc0e6e6230e285a8b2ac97d2bc508007
jmal <zhushilun084@gmail.com>
2023-03-15 14:55:06 +0800

    docs: 修改springdoc依赖

--- src/main/java/com/jmal/clouddisk/swagger/OpenApiConfig.java
+++ src/main/java/com/jmal/clouddisk/swagger/OpenApiConfig.java
@@ -3,15 +3,16 @@ package com.jmal.clouddisk.swagger;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.headers.Header;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.media.StringSchema;
 import io.swagger.v3.oas.models.parameters.HeaderParameter;
-import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 
 /**
@@ -22,31 +23,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
 
-    @Value("${version}")
-    String version;
-
     @Bean
-    public OpenAPI jmalCloudOpenApi() {
+    public OpenAPI jmalCloudOpenApi(@Value("${version}") String appVersion) {
 
         //在配置好的配置类中增加此段代码即可
 
         return new OpenAPI()
-                .components(new Components()
+                .components(new Components().addHeaders("jmal-token", new Header().description("myHeader2 header"))
                 .addParameters("myGlobalHeader", new HeaderParameter().required(true).name("jmal-token").description("").schema(new StringSchema()).required(false)))
                 .info(new Info().title("JmalCloud API")
                         .description("JmalCloud application")
-                        .version(version)
+                        .version(appVersion)
                         .contact(new Contact().name("jmal").url("https://blog.jmal.top").email("zhushilun084@gmail.com"))
                         .license(new License().name("MIT license").url("https://github.com/jamebal/jmal-cloud-view/blob/master/LICENSE")))
                         .externalDocs(new ExternalDocumentation());
     }
 
-    /**
-     * 添加全局的请求头参数
-     */
-    @Bean
-    public OpenApiCustomiser customerGlobalHeaderOpenApiCustomizer() {
-        return openApi -> openApi.getPaths().values().stream().flatMap(pathItem -> pathItem.readOperations().stream())
-                .forEach(operation -> operation.addParametersItem(new HeaderParameter().$ref("#/components/parameters/myGlobalHeader")));
-    }
+
 }
