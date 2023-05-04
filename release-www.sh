bab4dfbe9a6c1f62936a70355740d692f77e751a
jmal <zhushilun084@gmail.com>
2023-05-04 13:51:01 +0800

    ci: add script release-www.sh

---
+++ release-www.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# version
+if [ -n "$1" ]; then
+  echo "version: $1"
+else
+  echo "需要带上版本号, 例如 sh release-www.sh 2.5.1"
+  exit
+fi
+version=$1
+# jmal-cloud-view Directory location
+view_dir="/Users/jmal/studio/myProject/github/jmal-cloud-view"
+# jmal-cloud-server Directory location
+server_dir="/Users/jmal/studio/myProject/github/jmal-cloud-server"
+
+# build jmal-cloud-view
+cd $view_dir || exit
+echo "location: ${view_dir} "
+if [ ! -f "dist-$version.tar" ]; then
+  npm run build:prod
+  tar -czf "dist-$version.tar" dist
+fi
+echo "current $(pwd)"
+echo "copy dist-$version.tar to $server_dir/www/releases/dist-latest.tar"
+cp "dist-$version.tar" $server_dir"/www/releases/dist-latest.tar"
