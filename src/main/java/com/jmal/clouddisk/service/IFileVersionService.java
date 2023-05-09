68553faaccf909b67119546b759ee08f0a442dee
jmal <zhushilun084@gmail.com>
2023-05-09 18:00:47 +0800

    test: 文件版本管理

---
+++ src/main/java/com/jmal/clouddisk/service/IFileVersionService.java
@@ -0,0 +1,9 @@
+package com.jmal.clouddisk.service;
+
+/**
+ * @author jmal
+ * @Description 文件版本管理
+ * @date 2023/5/9 17:52
+ */
+public interface IFileVersionService {
+}
---
+++ src/main/java/com/jmal/clouddisk/service/impl/FileVersionServiceImpl.java
@@ -0,0 +1,13 @@
+package com.jmal.clouddisk.service.impl;
+
+import com.jmal.clouddisk.service.IFileVersionService;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author jmal
+ * @Description 文件版本管理
+ * @date 2023/5/9 17:53
+ */
+@Slf4j
+public class FileVersionServiceImpl implements IFileVersionService {
+}
