1121854721007caf0e53230118f5aa1cb210b4e4
jmal <zhushilun084@gmail.com>
2024-01-05 09:21:37 +0800

    chore: optimize jc.sh script reset password function

--- jc.sh
+++ jc.sh
@@ -277,8 +277,8 @@ backup_and_install() {
 
 reset_password() {
   local container_server="$(env_get CONTAINER_NAME_PREFIX)_server"
+  docker restart "$container_server"
   run_exec mongodb "mongo jmalcloud --eval \"db.getCollection('user').update({ 'creator': true }, {\\\$set: { 'password': '1000:c5b705ea13a1221f5e59110947ed806f8a978e955fbd2ed6:22508de12228c34a235454a0caf3bcaa5552858543258e56' }}, { 'multi': false, 'upsert': false })\" > /dev/null 2>&1"
-  docker restart "$container_server" > /dev/null 2>&1
   check_run "重置"
   echo -e "${OK} ${Green}重置后的密码为：jmalcloud${Font}"
 }
