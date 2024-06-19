30cbd666b92938246360ee2a2c20905c3969af1c
jmal <zhushilun084@gmail.com>
2024-06-19 12:13:11 +0800

    perf: 优化文本文件编码格式

--- src/main/java/com/jmal/clouddisk/util/MyFileUtils.java
+++ src/main/java/com/jmal/clouddisk/util/MyFileUtils.java
@@ -2,6 +2,8 @@ package com.jmal.clouddisk.util;
 
 import cn.hutool.core.io.CharsetDetector;
 import cn.hutool.core.io.FileTypeUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.mozilla.universalchardet.UniversalDetector;
 
 import java.io.File;
 import java.nio.charset.Charset;
@@ -14,6 +16,7 @@ import java.util.List;
  * @Description 文件工具类
  * @Date 2020-06-16 16:24
  */
+@Slf4j
 public class MyFileUtils {
 
     public static List<String> hasContentTypes = Arrays.asList("pdf", "ppt", "pptx", "doc", "docx", "drawio", "mind");
@@ -28,8 +31,11 @@ public class MyFileUtils {
      * @return 字符编码
      */
     public static Charset getFileCharset(File file) {
-        Charset charset = CharsetDetector.detect(file);
-        return charset == null ? StandardCharsets.UTF_8 : charset;
+        try {
+            return Charset.forName(UniversalDetector.detectCharset(file), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            return StandardCharsets.UTF_8;
+        }
     }
 
     public static boolean checkNoCacheFile(File file) {
