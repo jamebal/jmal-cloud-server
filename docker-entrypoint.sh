7a4898a1b82b9fe6de8feeaba8fce74156145020
jmal <zhushilun084@gmail.com>
2024-06-23 10:50:21 +0800

    chore: update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,21 +2,27 @@
 set -e
 
 # 获取传入的 UID 和 GID 环境变量
-USER_UID=${UID:-0}
-USER_GID=${GID:-0}
+USER_UID=${UID:-$(id -u)}
+USER_GID=${GID:-$(id -g)}
 
 # 检查是否使用 root 用户
 if [ "$USER_UID" -eq 0 ] && [ "$USER_GID" -eq 0 ]; then
     echo "Running as root"
+    exec "$@"
 else
     # 创建用户组和用户，并将其 UID 和 GID 设置为环境变量指定的值
-    groupadd -g $USER_GID appgroup || true
-    useradd -m -u $USER_UID -g appgroup appuser || true
+    if ! getent group appgroup > /dev/null 2>&1; then
+        groupadd -g $USER_GID appgroup || true
+    fi
+
+    if ! id -u appuser > /dev/null 2>&1; then
+        useradd -m -u $USER_UID -g appgroup appuser || true
+    fi
 
     # 更改挂载路径的所有者
     chown -R appuser:appgroup /jmalcloud
 
-    # 切换到新用户
+    # 切换到新用户并执行应用程序
     exec gosu appuser "$0" "$@"
 fi
 
