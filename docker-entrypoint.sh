2eb661342565a8101fe58f5f229c330e96dea46c
jmal <zhushilun084@gmail.com>
2024-06-23 14:21:40 +0800

    chore: update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,12 +2,26 @@
 set -e
 
 USER_UID=${PUID:-0}
+USER_GID=${PGID:-0}
 
-chown -R "${USER_UID}" /jmalcloud
+## 创建用户组和用户，并将其 UID 和 GID 设置为环境变量指定的值
+#if ! getent group appgroup > /dev/null 2>&1; then
+#    groupadd -g $USER_GID appgroup || true
+#fi
+#
+#if ! id -u appuser > /dev/null 2>&1; then
+#    useradd -m -u $USER_UID -g appgroup appuser || true
+#fi
 
-useradd --shell /bin/bash -u "${USER_UID}" -o -c "" -m user
-usermod -a -G root user
-export HOME=/home/user
+mkdir -p /log
+
+# 更改挂载路径的所有者
+chown -R ${USER_UID}:${USER_GID} /jmalcloud
+
+chown ${USER_UID}:${USER_GID} /usr/local/clouddisk-${VERSION}.jar
+chown -R ${USER_UID}:${USER_GID} /usr/local/clouddisk-lib
+chown -R ${USER_UID}:${USER_GID} log
+
 
 # 启动主应用程序
-exec gosu "${USER_UID}" java -Dfile.encoding=UTF-8 -Dloader.path=/usr/local/clouddisk-lib -jar ${JVM_OPTS} /usr/local/clouddisk-${VERSION}.jar --spring.profiles.active=${RUN_ENVIRONMENT} --spring.data.mongodb.uri=${MONGODB_URI} --tess4j.data-path=${TESS4J_DATA_PATH} --file.monitor=${FILE_MONITOR} --file.rootDir=${FILE_ROOT_DIR} --logging.level.root=${LOG_LEVEL} --file.ip2region-db-path=/jmalcloud/ip2region.xdb
+exec gosu ${USER_UID}:${USER_GID} java -Dfile.encoding=UTF-8 -Dloader.path=/usr/local/clouddisk-lib -jar ${JVM_OPTS} /usr/local/clouddisk-${VERSION}.jar --spring.profiles.active=${RUN_ENVIRONMENT} --spring.data.mongodb.uri=${MONGODB_URI} --tess4j.data-path=${TESS4J_DATA_PATH} --file.monitor=${FILE_MONITOR} --file.rootDir=${FILE_ROOT_DIR} --logging.level.root=${LOG_LEVEL} --file.ip2region-db-path=/jmalcloud/ip2region.xdb
