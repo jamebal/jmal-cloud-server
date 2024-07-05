d779b8496cef8e96c9f09184abbb17b671c1be25
jmal <zhushilun084@gmail.com>
2024-07-05 10:23:19 +0800

    perf: 优化获取真实ip

--- src/main/java/com/jmal/clouddisk/service/impl/LogService.java
+++ src/main/java/com/jmal/clouddisk/service/impl/LogService.java
@@ -119,7 +119,11 @@ public class LogService {
     }
 
     private String getIpAddress(HttpServletRequest request) {
-        String ip = request.getHeader("x-forwarded-for");
+        String ip = request.getRemoteHost();
+        if (CharSequenceUtil.isNotBlank(ip)) {
+            return ip;
+        }
+        ip = request.getHeader("x-forwarded-for");
         if (!CharSequenceUtil.isBlank(ip) && (ip.contains(","))) {
             // 多次反向代理后会有多个ip值，第一个ip才是真实ip
             ip = ip.split(",")[0];
@@ -141,9 +145,6 @@ public class LogService {
         }
         if (CharSequenceUtil.isBlank(ip)) {
             ip = request.getHeader("X-Real-IP");
-        }
-        if (CharSequenceUtil.isBlank(ip)) {
-            ip = request.getRemoteAddr();
         }
         return ip;
     }
