250016fc51359e939c59a660166234f5cdc3f997
jmal <zhushilun084@gmail.com>
2025-06-26 15:01:06 +0800

    refactor(webdav): 优化 WebdavConfig 类中的 dispatcherServletRegistration 方法
    - 调整代码格式，提高可读性- 添加方法参数的注释，增强代码文档性
    - 移除未使用的参数 multipartConfig，简化方法签名

--- src/main/java/com/jmal/clouddisk/webdav/WebdavConfig.java
+++ src/main/java/com/jmal/clouddisk/webdav/WebdavConfig.java
@@ -40,12 +40,13 @@ public class WebdavConfig {
      * <li>通过设置 loadOnStartup = 1 强制立即初始化，解决初始请求延迟问题。</li>
      * <li>注入并设置 MultipartConfigElement，以确保文件上传功能在不同环境下的稳定性，避免自动配置更改带来的影响。</li>
      * </ul>
+     *
      * @param dispatcherServlet Spring Boot 自动配置的 DispatcherServlet 实例。
-     * @param multipartConfig Spring Boot 根据 multipart 属性自动创建的 Multipart 配置。
+     * @param multipartConfig   Spring Boot 根据 multipart 属性自动创建的 Multipart 配置。
      * @return DispatcherServlet 的注册 Bean。
      */
     @Bean
-    public ServletRegistrationBean<DispatcherServlet> dispatcherServletRegistration( DispatcherServlet dispatcherServlet, MultipartConfigElement multipartConfig) {
+    public ServletRegistrationBean<DispatcherServlet> dispatcherServletRegistration(DispatcherServlet dispatcherServlet, MultipartConfigElement multipartConfig) {
         ServletRegistrationBean<DispatcherServlet> registration = new ServletRegistrationBean<>(dispatcherServlet);
         // 1. 确保处理根路径
         registration.addUrlMappings("/");
