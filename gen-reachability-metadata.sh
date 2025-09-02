844126951316c38cf3619a6f198f14e6caaf0344
jmal <zhushilun084@gmail.com>
2025-09-02 22:02:36 +0800

    feat: update Docker entrypoint and metadata scripts for improved logging and MongoDB integration

--- gen-reachability-metadata.sh
+++ gen-reachability-metadata.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+mongosh --eval "db.getSiblingDB('jmalcloud-test').dropDatabase()"
+
 JAR_NAME=$(ls target/jmalcloud-*.jar | sort -V | tail -n 1)
 
-java -agentpath:${GRAALVM_HOME}/lib/libnative-image-agent.dylib=config-output-dir=src/main/resources/META-INF/native-image -jar "$JAR_NAME"
-
+java -agentpath:${GRAALVM_HOME}/lib/libnative-image-agent.dylib=config-output-dir=src/main/resources/META-INF/native-image -jar "$JAR_NAME" \
+ --spring.profiles.active='dev, mongodb' \
+ --server.port=8099 \
+ -Duser.timezone=${TZ} \
+ -Dfile.encoding=UTF-8 \
+ --spring.data.mongodb.uri=mongodb://127.0.0.1:27017/jmalcloud-test \
+ --file.exactSearch=true \
+ --file.rootDir=/Users/jmal/temp/filetest/rootpath-test \
