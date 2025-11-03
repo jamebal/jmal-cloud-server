db316d5437530ff84479b38382f2ecf2aeeb6bc0
jmal <zhushilun084@gmail.com>
2025-11-03 14:45:05 +0800

    fix: increase timeout durations and enhance artifact handling in native build workflow

--- src/main/java/com/jmal/clouddisk/util/SystemUtil.java
+++ src/main/java/com/jmal/clouddisk/util/SystemUtil.java
@@ -49,7 +49,7 @@ public class SystemUtil {
 
         try (HttpClient httpClient = HttpClient.newBuilder()
                 .followRedirects(HttpClient.Redirect.NEVER)
-                .connectTimeout(Duration.ofSeconds(5))
+                .connectTimeout(Duration.ofSeconds(15))
                 .build()) {
             for (String url : urls) {
                 try {
@@ -74,7 +74,7 @@ public class SystemUtil {
     private static String getLastVersion(HttpClient httpClient, String url) throws IOException, InterruptedException {
         HttpRequest altRequest = HttpRequest.newBuilder()
                 .uri(URI.create(url))
-                .timeout(Duration.ofSeconds(5))
+                .timeout(Duration.ofSeconds(15))
                 .header(HttpHeaders.USER_AGENT, USER_AGENT)
                 .GET()
                 .build();
